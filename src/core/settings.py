2b8bcc5f983953ddf1f791cc4119eb0d5f04e0ba
Yurii Holovchenko <88655907+Yuriy1710@users.noreply.github.com>
2024-07-25 20:10:31 +0200

    Added vendors

--- src/core/settings.py
+++ src/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django_extensions',
     
     'visits',
+    'commando',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / 'vendors'
+
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+STATIC_ROOT = BASE_DIR / "local-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
