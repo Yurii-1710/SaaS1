46eddac7ca533e826bd606bc1109d69619567df3
Yurii Holovchenko <88655907+Yuriy1710@users.noreply.github.com>
2024-07-25 20:22:39 +0200

    Updated build args

--- src/core/settings.py
+++ src/core/settings.py
@@ -15,7 +15,6 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool)
-print("Debug", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app"
@@ -84,7 +83,7 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
-DATABASE_URL = config('DATABASE_URL', cast=str)
+DATABASE_URL = config('DATABASE_URL', default=None)
 if DATABASE_URL is not None:
     import dj_database_url
     DATABASES = {
