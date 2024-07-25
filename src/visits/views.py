473c5934b8bbe5dd00f37ee34c02d443e7675c85
Yurii Holovchenko <88655907+Yuriy1710@users.noreply.github.com>
2024-07-25 10:32:58 +0200

    Start

---
+++ src/visits/views.py
@@ -0,0 +1,15 @@
+
+from django.shortcuts import render
+from .models import *
+
+
+def home(request, *args, **kwargs):
+    title = "Fuck you"
+    queryset = PageVisits.objects.all()
+    context = {
+      'title': title,
+      'queryset': queryset.count()
+    }
+    template = 'home.html'
+    PageVisits.objects.create()
+    return render(request, template, context)
